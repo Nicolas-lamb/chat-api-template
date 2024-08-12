0441f2c5a8e8db3437d1f5fed2e77147d3db3add
nicolas-lamb <nikelamb@gmail.com>
2024-08-12 16:18:19 -0300

    primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let nome = document.querySelector("#inome")
+
+window.onload = function(){ 
+ nome.focus(); 
+ 
+}
+
+function Input_event(){
+    if(nome.value == "")
+        nome.style.textAlign = 'left'
+    else{
+        nome.style.textAlign = 'center'
+    }
+    
+}
