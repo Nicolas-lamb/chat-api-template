285edf3cfd4a5b17b5d7e58bac4afcb85e3d7110
JeanRauta <j34nwr@gmail.com>
2024-08-31 11:41:45 -0300

    up

--- scripts/index.js
+++ scripts/index.js
@@ -12,8 +12,8 @@ function Input_event(){
     }
     if(nome.value.length >= 3){
         document.querySelector('img').classList.add('act')
-        console.log(document.querySelector('img').classList)
         nome.style.color = "white"
+       
     }else{
         document.querySelector('img').classList.remove('act')
         nome.style.color = "rgba(246, 239, 226, 0.514)"
@@ -23,3 +23,9 @@ function Input_event(){
 function sub(){
     document.querySelector('img').classList.remove('act')
 }
+
+document.querySelector('img').addEventListener('click', () => {
+    if (nome.value.length >= 3) {
+        document.querySelector('form').submit();
+    }
+});
